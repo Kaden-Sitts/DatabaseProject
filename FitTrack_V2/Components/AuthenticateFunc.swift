3d6ab389ef41e2c18737b8ffb28b2df211d85219
Kaden Sitts <kadensitts@gmail.com>
2024-04-24 22:37:43 -0600

    Modifed the code so it should now interact with the db i created, need to create a sign in view next

--- FitTrack_V2/Components/AuthenticateFunc.swift
+++ FitTrack_V2/Components/AuthenticateFunc.swift
@@ -9,34 +9,32 @@ import Foundation
 import SwiftUI
 import SQLite
 
-
-func authenticate (username: String, password: String, message: inout String, isLoggedIn: inout Bool) {
-    let fileManager = FileManager.default
-    let databaseFileName = "mydatabase.sqlite"
-    let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
-    let databaseURL = documentsURL.appendingPathComponent(databaseFileName)
+func authenticate(username: String, password: String, message: inout String, isLoggedIn: inout Bool) {
+    guard let path = Bundle.main.path(forResource: "fit_track_db", ofType: "sqlite") else {
+        message = "Database file not found"
+        isLoggedIn = false
+        return
+    }
 
     do {
-        let db = try Connection(databaseURL.path)
-        let users = Table("users")
-        let savedUsername = Expression<String>("username")
-        let savedPassword = Expression<String>("password")
+        let db = try Connection(path)
+        let users = Table("Users")
+        let savedUsername = Expression<String>("Username")
+        let savedPassword = Expression<String>("Password")
 
         let query = users.filter(savedUsername == username && savedPassword == password)
 
         if let _ = try db.pluck(query) {
             isLoggedIn = true
-            // Message that log in should be successful
-            message = "log in was successful"
+            message = "Log in was successful"
         } else {
             isLoggedIn = false
-            // Message that it didnt work
             message = "Incorrect Username or Password"
         }
     } catch {
         isLoggedIn = false
-        // Message that error occured
-        message = "Error with logging in"
+        message = "Error with logging in: \(error)"
     }
 }
+
 
